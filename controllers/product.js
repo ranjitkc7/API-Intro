6d6cd418a1f043470db636e007237c10536920f8
ranjitkc7 <ranjit252@gmail.com>
2025-06-28 16:58:51 +0545

    fourth-commit

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,33 @@
 const Product = require("../models/products");
 
 const getAllProducts = async (req, res) => {
-  const { company } = req.query;
+  const { company, name, featured, sort, select } = req.query;
   const queryObject = {};
-
   if (company) {
-    queryObject.company = company;
+    queryObject.company = { $regex: company, $options: "i" };
   }
-  const myProducts = await Product.find(queryObject);
-  res.status(200).json({ myProducts });
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  let apiData = Product.find(queryObject).collation({ locale: "en" });
+  if (sort) {
+    let sortFix = sort.replace(",", " ");
+    apiData = apiData.sort(sortFix);
+  }
+  if(select){
+   let selectFix = select.split(",").join(" ");
+   apiData = apiData.select(selectFix);
+  }
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+  let skip = (page - 1)*limit;
+  apiData = apiData.skip(skip).limit(limit);
+  console.log(queryObject);
+  const myProducts = await apiData;
+res.status(200).json({ myProducts , nbHits: myProducts.length });
 };
 
 const getTest = async (req, res) => {
